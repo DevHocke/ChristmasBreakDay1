98adea201dcae9463d1d1aa8720e2d6fdc097626
Håkan Eriksson <badfish.paint@gmail.com>
2020-12-21 10:52:25 +0100

    Add project files.

---
+++ ChristmasBreakDay1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace ChristmasBreakDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello Håkan!");
+
+            //string one = "Damn";
+            //string two = "cat";
+            //string three = "get";
+            //string four = "off";
+            //string five = "my food!";
+
+            //int numb1 = 1;
+            //int numb2 = 2;
+            //int numb3 = 3;
+            //int numb4 = 4;
+            //int numb5 = 5;
+
+            //Console.WriteLine($"{one} {numb1}");
+
+
+        }
+    }
+}
